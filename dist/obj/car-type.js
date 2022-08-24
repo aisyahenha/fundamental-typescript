fe4b4c8caa0636c0118d5e5efb3ce512087f2950
Aisyahenha <aisyahnurulhamidah@gmail.com>
2022-08-24 15:14:54 +0700

    move from another repo in my pc

---
+++ dist/obj/car-type.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CarType;
+(function (CarType) {
+    CarType["Sedan"] = "Sedan";
+    CarType["Truck"] = "Truck";
+    CarType["Bus"] = "Bus";
+    CarType["Ferary"] = "Ferary";
+})(CarType || (CarType = {}));
+exports.default = CarType;
