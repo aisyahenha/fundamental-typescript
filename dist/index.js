76cd6e291df6029c0015a20433cae853107bd12d
Aisyahenha <aisyahnurulhamidah@gmail.com>
2022-08-25 10:09:01 +0700

    [RESPONSE and Pagination]

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,5 @@
 "use strict";
-function getArray(items) {
-    return new Array().concat(items);
+class SingleResponse {
 }
-let myNumArr = getArray([100, 200, 300]);
-let myStrArr = getArray(["Hello", "World"]);
-myNumArr.push(400);
-myStrArr.push("Hello TypeScript");
-myNumArr.push("Hi");
-myStrArr.push(500);
-console.log(myNumArr);
-console.log(myStrArr);
-function getData(items) {
-    return new Array().concat(items);
+class PagedResponse {
 }
-let dadu = getData([1, 2, 3, 4, 5]);
-let pejudi = getData(["Yuki", "Suho"]);
-dadu.push(6);
-pejudi.push("Soya");
-const employeee = getData([
-    {
-        name: "cecep",
-        age: 12,
-    },
-    {
-        name: "Aicep",
-        age: 15,
-    },
-]);
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,23 @@
-function getArray(items: any[]): any[] {
-  return new Array().concat(items);
-  //return [].concat(items); //same
+interface Paging{
+  page: number;
+  rowsPerPage: number;
+  totalRows: number;
+  totalPage: number;
+}
+interface ResponEntity<T> {
+  code: number;
+  message: string;
+  data: T;
+}
+class SingleResponse<T> implements ResponEntity<T> {
+  code: number;
+  message: string;
+  data: T;
 }
 
-let myNumArr = getArray([100, 200, 300]);
-let myStrArr = getArray(["Hello", "World"]);
-
-myNumArr.push(400); // OK
-myStrArr.push("Hello TypeScript"); // OK
-
-myNumArr.push("Hi"); // OK
-myStrArr.push(500); // OK
-
-console.log(myNumArr); // [100, 200, 300, 400, "Hi"]
-console.log(myStrArr); // ["Hello", "World", "Hello TypeScript", 500]
-
-/** using generik   */
-
-function getData<T>(items: T[]): T[] {
-  return new Array<T>().concat(items);
+class PagedResponse<T> implements ResponEntity<T> {
+  code: number;
+  message: string;
+  data: T;
+  paging: Paging;
 }
-
-let dadu = getData<number>([1, 2, 3, 4, 5]);
-let pejudi = getData<string>(["Yuki", "Suho"]);
-
-dadu.push(6); // OK
-pejudi.push("Soya"); // OK
-
-// dadu.push("Hi"); // Compiler Error
-// pejudi.push(500); // Compiler Error
-
-// console.log(dadu); // [100, 200, 300, 400, "Hi"]
-// console.log(myStrArr2); // ["Hello", "World", "Hello TypeScript", 500]
-
-/** source
- * https://www.tutorialsteacher.com/typescript/typescript-generic
- */
-
-type Employee = {
-  name: string;
-  age: number;
-  skils?: string[];
-};
-const employeee: Employee[] = getData<Employee>([
-  {
-    name: "cecep",
-    age: 12,
-  },
-  {
-    name: "Aicep",
-    age: 15,
-  },
-]);
